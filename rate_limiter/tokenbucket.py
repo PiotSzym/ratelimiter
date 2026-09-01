29ff4812c586cc83e2a49790db5465c45b70ec8b
PiotSzym <piotszym123@gmail.com>
2026-09-01 14:13:32 +0100

    feat: add token checking code

--- rate_limiter/tokenbucket.py
+++ rate_limiter/tokenbucket.py
@@ -5,6 +5,43 @@ class TokenBucketRateLimiter():
     Each request uses 1 token. Once all tokens are depleted, wait until bucket refills.
     """
 
-    def __init__(self, tokens_max: int, refill_rate: int):
+    def __init__(self, tokens_max: int, refill_rate: float) -> None:
         self.tokens_max = tokens_max
         self.refill_rate = refill_rate
+        self._buckets = {}
+        """
+        The underscore is used to encapsulate buckets for each user
+        """
+
+    def tokencheck(self, id:str) -> bool:
+        """
+        Init bucket for user if there isnt one already
+        Refill the bucket if enough time has passed
+        Check if the bucket has enough tokens
+        Return bool whether token was used or not enough tokens available
+        """
+
+        timenow = time.time()
+
+        if id not in self._buckets:
+            self._buckets[id] = {
+                "tokens": self.tokens_max,
+                "latest_refill": timenow
+            }
+            time_since_last_refill = 0
+        else:
+            time_since_last_refill = timenow - bucket["latest_refill"]
+
+        bucket = self._buckets[id]
+
+        tokens_now = min(self.tokens_max, bucket["tokens"] + time_since_last_refill*self.refill_rate)
+
+        bucket["tokens"] = tokens_now
+
+        bucket["latest_refill"] = timenow
+
+        if bucket["tokens"] < 1:
+            return False
+
+        bucket["tokens"] -= 1
+        return True
