6142c85665f8dd2ab957d48b453a6a2d4b84ff95
PiotSzym <piotszym123@gmail.com>
2026-09-02 15:24:05 +0100

    refactor: change var names

--- rate_limiter/mainclass.py
+++ rate_limiter/mainclass.py
@@ -4,12 +4,8 @@ from random import randint
 class RateLimiter():
 
     def __init__(self):
-        self.__id = str("ID",randint(1,1000000000))
+        self._id = str("ID",randint(1,1000000000))
         self.bucket = TokenBucketRateLimiter(5,1)
-
-    """
-    __ makes the id private so that it cant be changed by the user
-    """
 
     def allow(self, id: str, bucket: TokenBucketRateLimiter) -> bool:
         return bucket.tokencheck(id)
