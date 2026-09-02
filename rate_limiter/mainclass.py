7162a8fb57b1f5896c04f0bfae9fcbd36c52fe09
PiotSzym <piotszym123@gmail.com>
2026-09-02 15:19:11 +0100

    feat: small changes

--- rate_limiter/mainclass.py
+++ rate_limiter/mainclass.py
@@ -1,8 +1,15 @@
+from tokenbucket import TokenBucketRateLimiter
+from random import randint
+
 class RateLimiter():
 
-    def allow(self) -> bool:
-        """
-        Return True if the user is not rate limited otherwise false.
-        """
-        
-        return True
+    def __init__(self):
+        self.__id = str("ID",randint(1,1000000000))
+        self.bucket = TokenBucketRateLimiter(5,1)
+
+    """
+    __ makes the id private so that it cant be changed by the user
+    """
+
+    def allow(self, id: str, bucket: TokenBucketRateLimiter) -> bool:
+        return bucket.tokencheck(id)
