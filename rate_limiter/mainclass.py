6b0075b7c1da5ffb192f0e77d1f1c3c3b98bb737
PiotSzym <piotszym123@gmail.com>
2026-09-08 14:17:27 +0100

    bugfix

--- rate_limiter/mainclass.py
+++ rate_limiter/mainclass.py
@@ -4,7 +4,8 @@ from random import randint
 class RateLimiter():
 
     def __init__(self):
-        self._id = str("ID",randint(1,1000000000))
+        randomnumber = str(randint(1,1000000000))
+        self._id = str("ID",randomnumber)
         self.bucket = TokenBucketRateLimiter(5,1)
 
     def allow(self, id: str, bucket: TokenBucketRateLimiter) -> bool:
